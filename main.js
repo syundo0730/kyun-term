fb4751f3f855f544da60d8de8e9ffea9d98ab097
syundo0730 <syundo0730@gmail.com>
2016-11-13 19:48:22 +0900

    set up menu items

--- main.js
+++ main.js
@@ -1,21 +1,80 @@
 'use strict'
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { app, Menu, BrowserWindow } = require('electron')
 
 let mainWindow
 
-app.on('window-all-closed', function() {
+const template = [
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click() { require('electron').shell.openExternal('http://electron.atom.io') }
+      }
+    ]
+  }
+]
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  })
+}
+
+app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
     app.quit()
   }
 })
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+app.on('ready', function () {
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu)
+
+  mainWindow = new BrowserWindow({ width: 800, height: 600, title: 'KyunTerm' })
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     mainWindow = null
   })
 })
