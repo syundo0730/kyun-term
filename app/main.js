aa5ca58231be4697e681a900f79d6f564c8b6d32
syundo0730 <syundo0730@gmail.com>
2016-11-27 00:50:28 +0900

    added mechanism for auto update

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 'use strict'
-const { app, Menu, BrowserWindow } = require('electron')
+if(require('electron-squirrel-startup')) return;
+
+const { app, Menu, BrowserWindow, dialog, autoUpdater } = require('electron')
 
 let mainWindow
 
@@ -20,13 +22,15 @@ const template = [
     submenu: [
       {
         label: 'Learn More',
-        click() { require('electron').shell.openExternal('http://electron.atom.io') }
+        click() { require('electron').shell.openExternal('https://github.com/syundo0730/kyun-term') }
       },
       {
         label: 'Toggle Developer Tools',
         accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
         click (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools()
+          }
         }
       }
     ]
@@ -68,6 +72,16 @@ if (process.platform === 'darwin') {
   })
 }
 
+const appVersion = require('./package.json').version
+const baseUrl = 'https://kyun-term-update-server.herokuapp.com/update' 
+let feedURL = ''
+if (process.platform === 'darwin') {
+  feedURL = `${baseUrl}/osx/${appVersion}`
+} else if (process.platform === 'win32') {
+  feedURL = `${baseUrl}/win32/${appVersion}`
+}
+autoUpdater.setFeedURL(feedURL);
+
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
     app.quit()
@@ -84,4 +98,41 @@ app.on('ready', function () {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
+  mainWindow.webContents.on('did-frame-finish-load', function () {
+    autoUpdater.checkForUpdates();
+  });
+
+  autoUpdater.addListener("update-available", function(event) {
+    dialog.showMessageBox(
+      mainWindow,
+      {
+        message: 'A new update is available',
+        detail: 'Would you like to download?',
+        buttons: ['OK', 'Cancel']
+      },
+      function(index) {
+        if (index !== 0) return;
+        autoUpdater.addListener('update-downloaded', function(event, releaseNotes, releaseName, releaseDate, updateURL) {
+          dialog.showMessageBox(
+            mainWindow,
+            {
+              message: 'A new update is ready to install',
+              detail: `Version ${releaseName} is downloaded and will be automatically installed after restart`,
+              buttons: ['OK']
+            }
+          );
+        })
+      });
+  });
+
+  autoUpdater.addListener('error', function(error) {
+    dialog.showMessageBox(
+      mainWindow,
+      {
+        message: 'Error',
+        detail: error.toString(),
+        buttons: ['OK']
+      });
+  });
 })
